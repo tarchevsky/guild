15c4a37ccf68d87429602900667cec19733e29f8
tarchevsky <i.tarchevsky@gmai.com>
2024-07-19 20:03:28 +0300

    Правка размеров шрифтов, отступов, добавление ссылок в footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,15 +6,19 @@ import styles from './Footer.module.scss'
 const Footer = () => {
 	return (
 		<footer className='footer footer-center p-10 gap-y-1'>
-			<div className={cn(styles.logo)}>гильдия.москва</div>
-			<div>Дополняем и совершенствуем интерьеры.</div>
-			<Link href='tel:+79031700586' className='text-3xl py-4'>
-				7 903 170 05 86
+			<Link href='/' className={cn(styles.logo, 'md:mb-20')}>
+				гильдия.москва
 			</Link>
-			<Link href='mailto:guild.moscow@yandex.ru'>guild.moscow@yandex.ru</Link>
-			<div>Москва, Проспект Мира 68</div>
+			<div className='md:text-xl'>Художественный дизайн</div>
+			<Link href='tel:+79031700586' className='text-3xl py-4'>
+				+7 903 170 05 86
+			</Link>
+			<Link href='mailto:guild.moscow@yandex.ru' className='md:text-xl'>
+				guild.moscow@yandex.ru
+			</Link>
+			<div className='md:text-xl'>Москва, Проспект Мира 68</div>
 			<aside>
-				<p>Copyright © 2024 - разработкаТарчевский И.А.</p>
+				<p>Copyright © 2024 - разработка tezis.digital</p>
 			</aside>
 		</footer>
 	)
