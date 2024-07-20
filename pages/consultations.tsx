baf572519b5139e71c429305f20c3ccae665e921
tarchevsky <i.tarchevsky@gmai.com>
2024-07-20 16:17:02 +0300

    Вёрстка страниц consultations и contacts - образец вёртски modernBtn, параграфов и ul>li

--- pages/consultations.tsx
+++ pages/consultations.tsx
@@ -2,6 +2,8 @@ import { NextPage } from 'next'
 import Meta from '@/components/meta/Meta'
 import PageHeading from '@/components/pageHeading/PageHeading'
 import ModernBtn from '@/components/modernBtn/ModernBtn'
+import Link from 'next/link'
+import FadeIn from '@/components/fadeIn/FadeIn'
 
 const title = 'Консультации'
 const background =
@@ -14,8 +16,29 @@ const ConsultationsPage: NextPage = () => {
 				title={title}
 				metaDesc='Страница про консультации. Здесь мы описываем алгоритм обращения к нам и получения консультаций'
 			/>
-			<ModernBtn text='заказать <br />консультацию' background={background} />
 			<PageHeading title={title} background={background} />
+			<FadeIn className='relative z-10 cont mt-[32svh] text-2xl xl:text-3xl 2xl:text-4xl xl:max-w-5xl'>
+				<p className='mb-10'>
+					Мы предоставляем услуги консультаций. Наши сотрудники готовы ответить
+					на любые Ваши вопросы, связанные с искусством и дизайном
+				</p>
+				<ul className='flex flex-col gap-y-5 list-disc pl-7 pb-10'>
+					<li>Анализ и корректировка проекта с художественной точки зрения.</li>
+					<li>Консультация по подбору и оценке объектов искусства</li>
+					<li>Обеспечение контакта клиента с галлереями и арт-диллерами.</li>
+					<li>Помощь в разработке концепции проекта.</li>
+					<li>Консультация по подбору мебели и элементов декора интерьера.</li>
+				</ul>
+				<p className='mb-10'>
+					Первый ознакомительный час консультации за наш счёт. Расценки
+					высылаются по запросу.
+				</p>
+			</FadeIn>
+			<ModernBtn
+				text='заказать <br />консультацию'
+				background={background}
+				className='my-[5svh] xl:-mt-[15svh] z-10'
+			/>
 		</>
 	)
 }
--- pages/contacts.tsx
+++ pages/contacts.tsx
@@ -4,6 +4,7 @@ import Meta from '@/components/meta/Meta'
 import ContactForm from '@/components/contactForm/ContactForm'
 import PageHeading from '@/components/pageHeading/PageHeading'
 import ModernBtn from '@/components/modernBtn/ModernBtn'
+import Link from 'next/link'
 
 const title = 'Контакты'
 const background =
@@ -14,7 +15,31 @@ const Contacts: NextPage = () => {
 		<>
 			<Meta title={title} metaDesc='Описание страницы контактов' />
 			<PageHeading title={title} background={background} />
-			<ModernBtn text='Связаться с нами' background={background} />
+			<FadeIn className='relative z-10 cont mt-[32svh] text-2xl xl:text-3xl 2xl:text-4xl'>
+				<p className='mb-10'>
+					Тг: <Link href='t.me/guild.moscow'>t.me/guild.moscow</Link>
+				</p>
+				<p className='mb-10'>
+					Вк: <Link href='vk.com/guild.moscow33'>vk.com/guild.moscow33</Link>
+				</p>
+				<p className='mb-10'>
+					Инст: <Link href='@vasiliy.kozlovskiy'>@vasiliy.kozlovskiy</Link>
+				</p>
+				<p className='mb-10'>
+					Тел.: <Link href='tel:+79031700586'>+79031700586</Link>
+				</p>
+				<p className='mb-10'>
+					Почта:{' '}
+					<Link href='mailto:guild.moscow@yandex.ru'>
+						guild.moscow@yandex.ru
+					</Link>
+				</p>
+			</FadeIn>
+			<ModernBtn
+				text='Связаться с нами'
+				background={background}
+				className='mt-[10svh] md:mt-0 xl:-mt-[19svh] z-10'
+			/>
 		</>
 	)
 }
