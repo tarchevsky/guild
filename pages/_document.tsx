80c897c9631fdc016e64ee408035380a2798c956
tarchevsky <i.tarchevsky@gmai.com>
2024-07-26 19:22:17 +0300

    счетчик почему-то не работает, пробую ещё один вариант

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,8 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import Metrika from '@/components/metrika/Metrika'
+import Script from 'next/script'
+
+const yId = '97927647'
 
 export default function Document() {
 	return (
@@ -8,7 +11,25 @@ export default function Document() {
 			<body>
 				<Main />
 				<NextScript />
-				<Metrika yId='97927647' />
+				<Script
+					strategy='afterInteractive'
+					dangerouslySetInnerHTML={{
+						__html: `
+                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+                ym(${yId}}, "init", {
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true,
+                  webvisor:true
+                });
+              `
+					}}
+				/>
 			</body>
 		</Html>
 	)
