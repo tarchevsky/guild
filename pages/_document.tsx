91036d3cc576ab5681cfcd168d84f5177814bc37
tarchevsky <i.tarchevsky@gmai.com>
2024-07-26 18:44:58 +0300

    Добавил метрику в компоненте

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Metrika from '@/components/metrika/Metrika'
 
 export default function Document() {
 	return (
@@ -7,6 +8,7 @@ export default function Document() {
 			<body>
 				<Main />
 				<NextScript />
+				<Metrika yId={97927647} />
 			</body>
 		</Html>
 	)
