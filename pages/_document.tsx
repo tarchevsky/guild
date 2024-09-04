478416eaef7e37b9f7e251d90431c50309576757
tarchevsky <i.tarchevsky@gmai.com>
2024-09-04 17:20:37 +0300

    Новый фавикон

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import link from 'next/link'
 
 export default function Document() {
 	return (
 		<Html lang='ru'>
-			<Head />
+			<Head>
+				<link rel='icon' href='/favicon.png' sizes='any' />
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
