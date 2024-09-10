35645087a3f2ed5d61aa72cd7fa002243744c717
tarchevsky <i.tarchevsky@gmai.com>
2024-09-10 14:38:45 +0300

    На страницах с мобильными заголовками не показывался обычный заголовок - пофикшено

--- src/components/pageHeading/PageHeading.tsx
+++ src/components/pageHeading/PageHeading.tsx
@@ -22,7 +22,7 @@ const PageHeading = ({
 			<main>
 				<h1
 					className={cn(
-						`mt-[16vh] md:mt-[20svh] text-[54px] xl:text-[110px] font-normal uppercase text-right ${titleMob ? 'hidden mb:block' : null}`,
+						`mt-[16vh] md:mt-[20svh] text-[54px] xl:text-[110px] font-normal uppercase text-right ${titleMob ? 'hidden md:block' : null}`,
 						className
 					)}
 				>
