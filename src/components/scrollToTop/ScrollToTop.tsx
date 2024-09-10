24d42e4fadfa991861f0f390392fb9664b62945d
tarchevsky <i.tarchevsky@gmai.com>
2024-09-10 14:40:47 +0300

    Своя картинка в scrollToTop с добавлением картинок и переименованием одной

--- src/components/scrollToTop/ScrollToTop.tsx
+++ src/components/scrollToTop/ScrollToTop.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, FC } from 'react'
-import { IoTriangleOutline } from 'react-icons/io5'
 import { motion, AnimatePresence } from 'framer-motion'
+import Image from 'next/image'
 
 const ScrollToTop: FC = () => {
 	const [isVisible, setIsVisible] = useState<boolean>(false)
@@ -39,19 +39,25 @@ const ScrollToTop: FC = () => {
 				>
 					<motion.button
 						onClick={scrollToTop}
-						className='bg-primary'
+						className='bg-base-100'
 						whileHover={{ scale: 1.1 }}
 						whileTap={{ scale: 0.9 }}
 						aria-label='Кнопка наверх страницы'
 						style={{
 							border: 'none',
 							borderRadius: '50%',
-							padding: '20px',
+							padding: '10px',
 							cursor: 'pointer',
 							boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)'
 						}}
 					>
-						<IoTriangleOutline size={24} color='#fff' />
+						<Image
+							src={'/guild-icon-bg.png'}
+							alt={''}
+							width={40}
+							height={40}
+							className={'rounded-full'}
+						/>
 					</motion.button>
 				</motion.div>
 			)}
