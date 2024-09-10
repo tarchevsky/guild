3fde44cf7a3bb61eef5d8eab71ce7fe54b0df74f
tarchevsky <i.tarchevsky@gmai.com>
2024-09-10 11:48:19 +0300

    Консультации - мобильная версия заголовка в прошлом коммите, в текущем добавление в PageHeading titleMob

--- pages/consultations.tsx
+++ pages/consultations.tsx
@@ -17,11 +17,7 @@ const ConsultationsPage: NextPage = () => {
 				title={title}
 				metaDesc='Страница про консультации. Здесь мы описываем алгоритм обращения к нам и получения консультаций'
 			/>
-			<PageHeading
-				title={title}
-				background={background}
-				titleMob='Консуль <br />тации'
-			/>
+			<PageHeading title={title} background={background} titleMob={titleMob} />
 			<FadeIn className='relative z-10 cont mt-[32svh] text-2xl xl:text-3xl 2xl:text-4xl xl:max-w-5xl'>
 				<p className='mb-10'>
 					Мы предоставляем услуги консультаций. Наши сотрудники готовы ответить
